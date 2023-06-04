5db88c86a96531c9e3e4bdf989caa52e89a9f2e3
yavuzce <89185192+yavuzce@users.noreply.github.com>
2023-06-04 20:19:01 +0200

    Add files via upload

---
+++ hidden-search-widget/script.js
@@ -0,0 +1,7 @@
+const searchBox = document.getElementById("aramaCubugu");
+
+const icon = document.querySelector(".fa-solid");
+
+function iconTiklanma() {
+  searchBox.classList.toggle("click");
+}
